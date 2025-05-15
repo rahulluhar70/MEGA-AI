08ad0a1c2d44e11f54796e0b1a784ac741959e9b
rahulluhar70 <rahulpanchallohar0@gmail.com>
2025-05-15 09:30:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '917017659124';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '917017659124' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -19,9 +19,9 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.botname = process.env.BOTNAME || 'RAHUL PANCHAL';
+global.pairingNumber = process.env.BOT_NUMBER || '917017659124' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'yZJ3RBiQ#kaJx9kYV6XbGUerCGxJXIhgN_qnl0oHvfU-sslat9C4' ;  // put your session id here
 
 global.mods = []
 global.prems = []
